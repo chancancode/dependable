ebf1423f4c2ebabfdbc361b1c398e0d5bf5a5961
Godfrey Chan <godfreykfc@gmail.com>
2014-11-29 02:29:54 -0800

    Expire assets automatically based on a config value

--- config/initializers/custom_sprockets_engine.rb
+++ config/initializers/custom_sprockets_engine.rb
@@ -1,7 +1,11 @@
 require 'tilt'
 
+Dir.mkdir(File.join(Rails.root, 'tmp')) rescue nil
+
 class CustomTemplate < Tilt::Template
   VERSION = 1
+
+  CONFIG_FILE_PATH = File.join(Rails.root, 'tmp', 'custom.txt')
 
   self.default_mime_type = 'application/javascript'
 
@@ -9,6 +13,7 @@ class CustomTemplate < Tilt::Template
 
   def evaluate(context, locals, &block)
     context.depend_on(__FILE__)
+    context.depend_on(CONFIG_FILE_PATH)
 
     %{
         alert("Generated " + ((Date.now() - #{Time.now.to_i * 1000}) / 1000) + " seconds ago");
@@ -19,6 +24,6 @@ class CustomTemplate < Tilt::Template
   end
 end
 
+File.open( CustomTemplate::CONFIG_FILE_PATH, 'w' ) { |f| f.puts Rails.configuration.custom.inspect }
+
 Rails.application.assets.register_engine '.custom', CustomTemplate
-
-
